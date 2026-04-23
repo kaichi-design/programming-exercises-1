7e0bbfac59d88dd7f32c92dce59aee65e2e2277c
kaichi-design <eda.kaichi0702@gmail.com>
2026-04-23 15:11:09 +0900

    バグなし

---
+++ favolite.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	char favorite1[32] = "わたしの枝廣快知です";
+	char favorite2[32] = "ぶっかけうどん";
+	char favorite3[32] = "しっぽくうどん";
+	
+	printf("%s\n", favorite1);
+	printf("%s\n", favorite2);
+	printf("%s\n", favorite3);
+return 0;
+}
